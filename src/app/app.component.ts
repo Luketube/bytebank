caaa3ac60436900f8fd4494b60bab8b962ca2ff2
Lucas Farias <lucasfarias48@hotmail.com>
2022-08-31 19:13:13 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BytBank';
+
+  transferencias: any[] = [];
+
+  transferir($event: Object){
+
+    const transferencia = {...$event, data: new Date()};
+    
+    this.transferencias.push(transferencia);
+    
+  }
 }
+
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Output() aoTransferir = new EventEmitter();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log("Realizada nova transferencia")
+
+    const valoresEmitidos = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valoresEmitidos);
+
+    this.valor = 0;
+    this.destino = 0;
+  
+  }
+
+}
